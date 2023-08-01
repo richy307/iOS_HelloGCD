033a75c1c9dedb819000384cc11f2f18cd4c38bb
Richy <richy@webcomm.com.tw>
2023-08-01 20:44:32 +0800

    DispatchQueue.main, DispatchQueue.global()

--- iOS_HelloGCD/ViewController.swift
+++ iOS_HelloGCD/ViewController.swift
@@ -12,6 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let task1 = {
+            for i in 1...5 {
+                print("Task1: \(i)")
+            }
+        }
+        let task2 = {
+            for i in 1...5 {
+                print("Task2: \(i)")
+            }
+        }
+        
+        DispatchQueue.main.async(execute: task1)
+        DispatchQueue.main.async(execute: task2)
+//        DispatchQueue.global().async(execute: task1)
+//        DispatchQueue.global().async(execute: task2)
+        
     }
 
 
